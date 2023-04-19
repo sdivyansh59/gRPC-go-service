7425f1a265637aad4b3d07d2c4ffda64b36f3314
Divyansh Singh <sdivyansh59@gmail.com>
2023-04-19 17:28:33 +0530

    proto and server is completed, after this i will work on client

---
+++ client/main.go
@@ -0,0 +1,6 @@
+package main
+
+const (
+	port =":8080"
+)
+
---
+++ server/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/sdivyansh59/grpc_basic_service/proto"
+)
+
+const (
+	port = "8080"
+)
+
+type helloServer struct {
+	pb.GreetServiceServer
+}
+
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to start the server %v", err)
+	}
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
+	log.Printf("server started at %v", lis.Addr())
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to start: %v", err)
+	}
+	
+}
