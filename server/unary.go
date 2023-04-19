7b72d3ea8f62633313162c6785099233cc497940
Divyansh Singh <sdivyansh59@gmail.com>
2023-04-19 22:19:15 +0530

    unary server and client code is completed , but not runing

---
+++ server/unary.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"context"
+
+	pb "github.com/sdivyansh59/grpc_basic_service/proto"
+)
+
+
+func (s *helloServer) SayHello (ctx context.Context, req *pb.NoParam) (*pb.HelloResponse, error) {
+	return &pb.HelloResponse{
+		Message: "Hello",
+	}, nil
+} 
